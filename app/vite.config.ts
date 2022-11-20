687c309254f53e22c5fea16539f15c28462775f9
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-11-20 05:33:39 +0100

    prevent flashbangs by embeding backgroud color

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,25 @@
 import {fileURLToPath, URL} from 'node:url';
-
 import {defineConfig} from 'vite';
+import {ViteEjsPlugin} from "vite-plugin-ejs";
 import vue from '@vitejs/plugin-vue';
+import resolveConfig from 'tailwindcss/resolveConfig'
+import fs from 'fs';
+
+const tailwindConfig = resolveConfig(JSON.parse(fs.readFileSync('tsconfig.config.json', 'utf-8')))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteEjsPlugin({
+      getColor(name: string, value: number) {
+        if (tailwindConfig.theme?.colors) {
+          return (tailwindConfig.theme?.colors as Record<string, Record<string, string>>)[name][value];
+        }
+        return "#000";
+      }
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
