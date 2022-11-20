d58c7ff7739ba71f7a284a056e76fe2883c5e550
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-11-20 23:05:51 +0100

    add button transition

--- app/src/components/inputs/ButtonInput.vue
+++ app/src/components/inputs/ButtonInput.vue
@@ -20,7 +20,7 @@ const emit = defineEmits<{
 
 <template>
   <button :type="props.type" @click="e => emit('click', e)" :disabled="props.disabled || savingForm || loading"
-    class="inline-flex items-center justify-center text-center bg-primary-500 highlight-white/10
+    class="inline-flex items-center justify-center text-center bg-primary-500 highlight-white/10 transition-colors duration-150
       text-white rounded-md px-3 py-2 disabled:opacity-75 disabled:cursor-not-allowed hover:bg-primary-400">
     <slot></slot>
   </button>
