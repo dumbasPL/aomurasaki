499200c056b7c8713e643fc23d34e9a55e2db298
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-11-29 01:10:40 +0100

    add floating-vue

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,17 +1,22 @@
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import FloatingVue, {VTooltip} from 'floating-vue';
 import App from './App.vue';
 import router from './router';
 import './icons';
 
-import './assets/style.css';
+import './assets/style.scss';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
+FloatingVue.options.container = '#popovers';
+FloatingVue.options.themes.tooltip.delay.show = 0;
+
 app.component('font-awesome-icon', FontAwesomeIcon);
+app.directive('tooltip', VTooltip);
 
 app.mount('#app');
