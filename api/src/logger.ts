451c430a2584e96ed496b949485f9224e814e4ab
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-11-30 23:04:34 +0100

    Update dependencies

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -1,8 +1,8 @@
 
 import {Logger} from 'tslog';
 
-const logger: Logger = new Logger({
-  minLevel: 'debug',
+const logger = new Logger({
+  minLevel: 2, // 0: silly, 1: trace, 2: debug, 3: info, 4: warn, 5: error, 6: fatal
 });
 
 logger.debug('Logger initialized');
