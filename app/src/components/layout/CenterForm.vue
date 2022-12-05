4d611bb374343a46fea7c74355ad959c7c8c9928
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-12-05 02:20:09 +0100

    add class prop to center form

--- app/src/components/layout/CenterForm.vue
+++ app/src/components/layout/CenterForm.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+const props = defineProps<{
+  class?: string,
+}>();
 </script>
 
 <template>
   <div class="w-full flex justify-center">
-    <div class="flex-1 max-w-sm flex flex-col gap-8">
+    <div class="flex-1 max-w-sm flex flex-col gap-8" :class="props.class">
       <slot></slot>
     </div>
   </div>
